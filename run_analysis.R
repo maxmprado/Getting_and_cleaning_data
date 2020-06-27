4467c0cd0506f2cc20aa35f2fa59cbd92eb8ddf7
maxmprado <67024312+maxmprado@users.noreply.github.com>
2020-06-27 17:53:31 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,35 @@
-aljksdflaksjf
-adf
-asd
-fa
-sdf
-asf
+library(data.table)
+
+# Activities and Features
+activities <- fread("UCI HAR Dataset/activity_labels.txt", col.names = c("ActLabel", "Activity"))
+features <- fread("UCI HAR Dataset/features.txt", col.names = c("index", "Feat"))
+
+#Train Data
+Xtrain <- fread("UCI HAR Dataset/train/X_train.txt", col.names = features$Feat )
+WantFeats <-grep("-(mean|std)\\()",names(Xtrain))
+Xtrain <- Xtrain[,WantFeats, with = F]
+
+Ytrain <- fread("UCI HAR Dataset/train/y_train.txt", col.names = "ActLabel")
+Subtrain <- fread("UCI HAR Dataset/train/subject_train.txt", col.names = "Sub")
+
+train <- cbind(Subtrain,Ytrain,Xtrain)
+
+#Test Data
+Xtest <- fread("UCI HAR Dataset/test/X_test.txt", col.names = features$Feat)#[, featuresWanted, with = FALSE]
+WantFeats <-grep("-(mean|std)\\()",names(Xtest))
+Xtest <- Xtest[,WantFeats, with = F]
+
+Ytest <- fread("UCI HAR Dataset/test/y_test.txt", col.names = "ActLabel")
+Subtest <- fread("UCI HAR Dataset/test/subject_test.txt", col.names = "Sub")
+
+test <- cbind(Subtest,Ytest,Xtest)
+
+all.data <- rbind(train,test)
+View(all.data)
+
+
+mlt.data <- melt(all.data, id = c("Sub", "ActLabel"))
+combined <- dcast(mlt.data, Sub + ActLabel ~ variable, mean)
+
+fwrite(x = combined, file = "tidy.csv", quote = FALSE)
+
